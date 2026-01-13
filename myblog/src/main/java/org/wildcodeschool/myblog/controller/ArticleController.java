924575ffad11475fb67732fff3d4feba68c70d79
Yiroma <yiromaric@gmail.com>
2026-01-13 15:48:29 +0100

    add deleteArticle method (Delete of BREAD)

--- myblog/src/main/java/org/wildcodeschool/myblog/controller/ArticleController.java
+++ myblog/src/main/java/org/wildcodeschool/myblog/controller/ArticleController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,6 +66,16 @@ public class ArticleController {
 
         Article updatedArticle = articleRepository.save(article);
         return ResponseEntity.ok(updatedArticle);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable Long id) {
+        Article article = articleRepository.findById(id).orElse(null);
+        if (article == null) {
+            return ResponseEntity.notFound().build();
+        }
+        articleRepository.delete(article);
+        return ResponseEntity.noContent().build();
     }
 
 }
