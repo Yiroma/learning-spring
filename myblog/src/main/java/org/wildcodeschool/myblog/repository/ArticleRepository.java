62a157607222970e9b2806393a65db487b63cbfe
Yiroma <yiromaric@gmail.com>
2026-01-14 11:27:09 +0100

    add search methods custom queries

--- myblog/src/main/java/org/wildcodeschool/myblog/repository/ArticleRepository.java
+++ myblog/src/main/java/org/wildcodeschool/myblog/repository/ArticleRepository.java
@@ -1,8 +1,18 @@
 package org.wildcodeschool.myblog.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.wildcodeschool.myblog.model.Article;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-    
+
+    List<Article> findByTitle(String title);
+
+    List<Article> findByContentContaining(String content);
+
+    List<Article> findByCreatedAtAfter(LocalDateTime createdAt);
+
+    List<Article> findTop5ByOrderByCreatedAtDesc();
 }
