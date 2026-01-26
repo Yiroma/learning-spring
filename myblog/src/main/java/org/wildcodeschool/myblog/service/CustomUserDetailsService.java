2d074858fec1b15594f73441c07df0a7ccc894e5
Yiroma <yiromaric@gmail.com>
2026-01-26 19:32:10 +0100

    fix missing @Service annotation

--- myblog/src/main/java/org/wildcodeschool/myblog/service/CustomUserDetailsService.java
+++ myblog/src/main/java/org/wildcodeschool/myblog/service/CustomUserDetailsService.java
@@ -3,8 +3,10 @@ package org.wildcodeschool.myblog.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import org.wildcodeschool.myblog.repository.UserRepository;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
