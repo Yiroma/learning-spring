c41ca31a3ad10ede096fb0aa2e8349cb245923d3
Yiroma <yiromaric@gmail.com>
2026-01-26 17:11:45 +0100

    add missing import

--- myblog/src/main/java/org/wildcodeschool/myblog/security/AuthenticationService.java
+++ myblog/src/main/java/org/wildcodeschool/myblog/security/AuthenticationService.java
@@ -3,6 +3,7 @@ package org.wildcodeschool.myblog.security;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
